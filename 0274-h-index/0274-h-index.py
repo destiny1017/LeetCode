ee798581c924ff0d442b2bfef9aead52b2bad965
KimDaeho <eogh6428@naver.com>
2024-11-28 20:44:12 +0900

    Time: 0 ms (100.00%), Space: 17.5 MB (16.22%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -2,7 +2,6 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         answer = 0
         citations.sort()
-        pivot = 0
         for i in range(len(citations)):
             h = len(citations) - i
             if citations[i] >= h:
