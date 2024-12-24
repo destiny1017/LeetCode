625893a2903047e50b5d1c1bd59a61b98274ea95
KimDaeho <eogh6428@naver.com>
2024-12-24 21:17:00 +0900

    Time: 116 ms (5.01%), Space: 23.9 MB (6.16%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,17 +1,12 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         
-        i = 0
-        st = 0
-        ed = 0
-        tank = 0
+        size = len(gas)
+        st, ed, tank = 0, 0, 0   
+        arr = [gas[i] - cost[i] for i in range(size)]
         
-        arr = [gas[i] - cost[i] for i in range(len(gas))]
-        #print(arr)
-        
-        while st < len(gas) and ed - st < len(gas):
-            #print(st, ed, tank)
-            if tank + arr[ed % len(gas)] < 0:
+        while st < size and ed - st < size:
+            if tank + arr[ed % size] < 0:
                 if st < ed:
                     tank -= arr[st]
                     st += 1
@@ -19,10 +14,10 @@ class Solution:
                     st += 1
                     ed += 1
             else:
-                tank += arr[ed % len(gas)]
+                tank += arr[ed % size]
                 ed += 1
         
-        if tank >= 0 and st < len(gas):
+        if tank >= 0 and st < size:
             return st
         else:
             return -1
