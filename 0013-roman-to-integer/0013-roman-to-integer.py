8e47d6aee04b7a6508f8379b5f6ef1a816884a3b
KimDaeho <eogh6428@naver.com>
2024-12-26 20:57:28 +0900

    Time: 4 ms (65.64%), Space: 17.7 MB (7.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -7,21 +7,14 @@ class Solution:
                     'C': 100,
                     'D': 500,
                     'M': 1000}
-        comb_words = {'I': ['V', 'X'], 'X': ['L', 'C'], 'C': ['D', 'M']}
         
         sum, i = 0, 0
-        while i < len(s):
-            if s[i] in comb_words.keys() and i < len(s) - 1:
-                if s[i+1] == comb_words[s[i]][0]:
-                    sum += intmap[comb_words[s[i]][0]] - intmap[s[i]]
-                    i += 2
-                    continue
-                elif s[i+1] == comb_words[s[i]][1]:
-                    sum += intmap[comb_words[s[i]][1]] - intmap[s[i]]
-                    i += 2
-                    continue
-
-            sum += intmap[s[i]]
-            i += 1
+        for i in range(len(s) - 1):
+            if intmap[s[i]] < intmap[s[i+1]]:
+                sum -= intmap[s[i]]
+            else:
+                sum += intmap[s[i]]
+            
+        sum += intmap[s[-1]]
 
         return sum
