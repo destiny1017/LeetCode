e4f6602b15dd9852c3fb08bc210eb2540d7a254f
KimDaeho <eogh6428@naver.com>
2025-06-30 21:58:42 +0900

    Time: 0 ms (100%), Space: 17.9 MB (17.52%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,45 +5,37 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        sorted_node = Optional[ListNode]
+        head = ListNode(0, None)
+
         if list1 is None:
             return list2
-        elif list2 is None:
+        if list2 is None:
             return list1
         
-        merged_node = ListNode()
-        if list1.val <= list2.val:
-            merged_node.val = list1.val
+        if list1.val < list2.val:
+            head = ListNode(list1.val, None)
             list1 = list1.next
         else:
-            merged_node.val = list2.val
+            head = ListNode(list2.val, None)
             list2 = list2.next
         
-        self.exec_merge(merged_node, list1, list2)
+        next_node = head
         
-        return merged_node
-    
-    def exec_merge(self, node, list1, list2):
-        if list1 is None and list2 is None:
-            return
+        while True:
+            if list1 is None:
+                next_node.next = list2
+                break
+            if list2 is None:
+                next_node.next = list1
+                break
+            
+            if list1.val < list2.val:
+                next_node.next = ListNode(list1.val, None)
+                list1 = list1.next
+            else:
+                next_node.next = ListNode(list2.val, None)
+                list2 = list2.next
+            next_node = next_node.next
 
-        less_node = self.compare_node(list1, list2)
-        node.next = ListNode(val=less_node.val)
-
-        if less_node == list1:
-            list1 = list1.next
-        elif less_node == list2:
-            list2 = list2.next
+        return head
         
-        self.exec_merge(node.next, list1, list2)
-    
-    def compare_node(self, node1, node2):
-        if node1 is None:
-            return node2
-        if node2 is None:
-            return node1
-        
-        if node1.val <= node2.val:
-            return node1
-        else:
-            return node2
